c191b79cdf5b40ed8c1e37915254816c6f61c75c
sk4tma <g04004@ktc.ac.jp>
2026-02-25 22:32:00 +0900

    event

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <>
       <header>
-        
         <nav>
           {/*
           <Link href="/">Home</Link>
@@ -29,12 +28,6 @@ export default function Home() {
           </section>
 
           <PlayerTimeline/>
-
-          {/* playerセクション */}
-          <section id="player" className="px-2 py-2 mx-4 my-4 my-prpl-bg">
-            <h2 className="text-4xl text-center">Player</h2>
-            <p className="">年表</p>
-          </section>
 
           {/* setterセクション */}
           <section id="setter" className="px-2 py-2 mx-4 my-4 my-prpl-bg">
@@ -91,9 +84,19 @@ export function PlayerTimeline() {
     { year: '2020', month:'11' , competition: '日本ユース選手権 ボルダー', description: '葛飾 2位' ,type: "national", discipline: "boulder", detail: "0"},
   ]
   const [hoveredIndex, setHoveredIndex] = useState<number|null>(null)
+  const handleEventHover = (state: string, index: number) => {
+    switch(state){
+      case "enter":
+        setHoveredIndex(index);
+        break;
+      case "leave":
+        setHoveredIndex(null);
+        break;
+    }
+  }
 
   return (
-    <section className="my-prpl-bg py-16 m-4">
+    <section className="my-prpl-bg py-10 m-4">
       <div className="container mx-auto px-4 max-w-4xl">
         <h2 className="text-4xl font-bold text-center mb-12">主な戦歴</h2>
         
@@ -103,24 +106,20 @@ export function PlayerTimeline() {
           
           {/* イベント */}
           {events.map((event, index) => (
-            <div
-              key={index}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-              className=""
-            >
+            <div key={index}>
               <div key={index} className="mb-8 flex justify-between items-center items-stretch w-full bg-red-500/0">
                 {event.type === "national" ? (
                   <>
-                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"}/>
+                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
                     <PlayerTimeline_Dot/>
+                    <div></div>
                     <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
                   </>
                 ) : (
                   <>
                     <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
                     <PlayerTimeline_Dot/>
-                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"}/>
+                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
                   </>
                 )}
               </div>
@@ -134,9 +133,9 @@ export function PlayerTimeline() {
 
 export function PlayerTimeline_Detail({hovered, detail}: PlayerTimeline_DetailProps){
   return(
-    <div className="w-5/12 overflow-hidden">
+    <div className="w-5/12 overflow-hidden rounded-lg">
       {hovered ? (
-        <div className={"flex items-center justify-center relative h-full rounded-lg transition-all duration-300 cursor-pointer bg-white shadow-xl scale-105"}>
+        <div className={"flex items-center justify-center relative h-full transition-all duration-300 cursor-pointer bg-white shadow-xl scale-105"}>
           <p className={"mx-5 text-sm"}>{detail}</p>
         </div>
       ):(
@@ -153,16 +152,20 @@ interface PlayerTimeline_DetailProps{
 
 export function PlayerTimeline_Dot(){
   return(
-    <div className="w-2/12 flex justify-center">
+    <div className="w-2/12 flex justify-center items-center">
     <div className="w-4 h-4 bg-blue-500 rounded-full border-4 border-white shadow" />
     </div>
   )
 }
 
 
-export function PlayerTimeline_Event({year, month, competition, description, discipline, left}: PlayerTimeline_EventProps){
+export function PlayerTimeline_Event({year, month, competition, description, discipline, left, onHoverChange, index}: PlayerTimeline_EventProps){
   return(
-    <div className={`w-5/12 ${left?"text-right pl-8":"text-left pr-8"}`}>
+    <div
+    onMouseEnter={() => onHoverChange("enter", index)}
+    onMouseLeave={() => onHoverChange("leave", index)}
+    className={`w-5/12 ${left?"text-right pl-8":"text-left pr-8"}`}
+    >
       <h3 className={`text-2xl font-bold text-blue-600 ${discipline==='boulder'?"text-yellow-500":discipline==='lead'?"text-green-600":"text-red-400"}`}>{year + "." + month}</h3>
       <h4 className={`text-lg font-semibold mt-2`}>{competition}</h4>
       <p className="text-gray-600 mt-1">{description}</p>
@@ -178,4 +181,6 @@ interface PlayerTimeline_EventProps{
   description: string
   discipline: string
   left: boolean
+  onHoverChange: (state: string, index: number) => void
+  index: number
 }

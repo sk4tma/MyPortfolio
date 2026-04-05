be58ef8ac569c95ca598dd4a0474d30ef2272355
sk4tma <g04004@ktc.ac.jp>
2026-04-05 18:26:20 +0800

    Setter

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import './style.css';
 import { useState } from 'react';
+import { FaInstagram } from "react-icons/fa";
+import { SiThreads, SiNote } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -20,26 +22,25 @@ export default function Home() {
           <About/>
 
           {/* playerセクション */}
-          <PlayerTimeline/>
+          <Player/>
 
           {/* setterセクション */}
-          <section id="setter" className="my-prpl-bg section_detail">
-            <h2>Route Setter</h2>
-            <p className="">自己紹介文...</p>
-          </section>
+          <Setter/>
 
           {/* Projectsセクション */}
+          {/*
           <section id="projects" className="my-prpl-bg section_detail">
             <h2>Projects</h2>
             <div className="projects-grid">
               <article className="project-card">
-                {/* <img src="/project1.jpg" alt="プロジェクト1" /> */}
+                <img src="/project1.jpg" alt="プロジェクト1" />
                 <h3>プロジェクト名</h3>
                 <p>説明</p>
                 <a href="#">View More</a>
               </article>
             </div>
           </section>
+          */}
 
           {/* Contactセクション */}
           <Contact/>
@@ -58,8 +59,8 @@ export default function Home() {
 export function About(){
   return(
     <section id="about" className="my-prpl-bg section_detail">
-      <h2>About Me</h2>
-      <div className="mx-6 px-4 my-white-bg">
+      <h1>About Me</h1>
+      <div className="mx-6 px-4 py-4 my-white-bg">
         <div className="flex justify-center items-center">
           <div className="w-6/12 mx-4 my-4">
             <p className="noto-serif">　2006年6月19日生まれ。2015年にクライミングを始め、翌年からクライミングスクールで本格的に練習を始める。<br/>　ユースC(現在でいうU14)からユース選手権に出場し、リードユース選手権は2度目、ボルダーユース選手権は3度目で決勝に進出する。</p>
@@ -110,13 +111,38 @@ export function About(){
 export function Contact(){
   return(
     <section id="contact" className="my-prpl-bg section_detail">
-      <h2>Contact</h2>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message"></textarea>
-        <button type="submit">Send</button>
-      </form>
+      <h1>Contact</h1>
+      <div className="flex items-center gap-4">
+        <a
+          href="https://instagram.com/yusuke5875"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex h-10 w-10 items-center justify-center rounded-full text-gray-700 hover:text-pink-600 hover:bg-pink-50 transition"
+          aria-label="Instagramを開く"
+        >
+          <FaInstagram className="h-6 w-6" aria-hidden="true" />
+        </a>
+
+        <a
+          href="https://www.threads.net/@yusuke5875"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex h-10 w-10 items-center justify-center rounded-full text-gray-700 hover:text-pink-600 hover:bg-pink-50 transition"
+          aria-label="Instagramを開く"
+        >
+          <SiThreads className="h-6 w-6" aria-hidden="true" />
+        </a>
+
+        <a
+          href="https://note.com/ysk_5875"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex h-10 w-10 items-center justify-center rounded-full text-gray-700 hover:text-pink-600 hover:bg-pink-50 transition"
+          aria-label="Instagramを開く"
+        >
+          <SiNote className="h-6 w-6" aria-hidden="true" />
+        </a>
+      </div>
     </section>
   )
 }
@@ -238,20 +264,57 @@ export function Navbar(){
   )
 }
 
+export function Player(){
+  return(
+    <section id="player" className="my-prpl-bg section_detail">
+      <div className="container mx-auto px-4 max-w-4xl">
+        <h1>Player</h1>
+        <PlayerStyle/>
+        <PlayerTimeline/>
+      </div>
+    </section>
+  )
+}
+
+export function PlayerStyle() {
+
+  return (
+    <div className="my-white-bg my-6">
+      <div className="py-9">
+        <h2>Style</h2>
+      </div>
+      <div className="flex justify-center items-center">
+          <div className="w-6/12 mx-4 my-4">
+            <p className="noto-serif">　2006年6月19日生まれ。2015年にクライミングを始め、翌年からクライミングスクールで本格的に練習を始める。<br/>　ユースC(現在でいうU14)からユース選手権に出場し、リードユース選手権は2度目、ボルダーユース選手権は3度目で決勝に進出する。</p>
+          </div>
+          <div className="w-6/12 mx-4 my-4">
+            <Image
+              src="/images/2023_B_BJC_S1.jpg"
+              alt="説明"
+              width={600}
+              height={400}
+              priority
+            />
+          </div>
+        </div>
+    </div>
+  )
+}
+
 export function PlayerTimeline() {
   const events = [
-    { year: '2026', month:'2' , competition: 'ボルダージャパンカップ', description: '駒沢 7位' ,type: "national", discipline: "boulder", detail: "0"},
-    { year: '2025', month:'6' , competition: 'ボルダーワールドカップ', description: 'インスブルック 10位' ,type: "international", discipline: "boulder", detail: "0"},
-    { year: '2025', month:'2' , competition: 'ボルダージャパンカップ', description: '駒沢 4位' ,type: "national", discipline: "boulder", detail: "0"},
-    { year: '2024', month:'8' , competition: '世界ユース選手権 リード', description: '貴陽 1位' ,type: "international", discipline: "lead", detail: "0"},
-    { year: '2024', month:'5' , competition: '日本ユース選手権 リード', description: '盛岡 3位' ,type: "national", discipline: "lead", detail: "0"},
-    { year: '2023', month:'12' , competition: 'アジアカップ ボルダー', description: 'リヤド 3位' ,type: "international", discipline: "boulder", detail: "0"},
-    { year: '2023', month:'8' , competition: '世界ユース選手権 ボルダー', description: 'ソウル 4位' ,type: "international", discipline: "boulder", detail: "0"},
-    { year: '2023', month:'6' , competition: '日本ユース選手権 ボルダー', description: '倉吉 2位' ,type: "national", discipline: "boulder", detail: "0"},
-    { year: '2021', month:'12' , competition: '日本ユース選手権 ボルダー', description: '倉吉 1位' ,type: "national", discipline: "boulder", detail: "0"},
-    { year: '2021', month:'8' , competition: '世界ユース選手権 スピード', description: 'ヴォロネジ 3位' ,type: "international", discipline: "speed", detail: "0"},
-    { year: '2021', month:'3' , competition: '日本ユース選手権 スピード', description: '亀岡 1位' ,type: "national", discipline: "speed", detail: "0"},
-    { year: '2020', month:'11' , competition: '日本ユース選手権 ボルダー', description: '葛飾 2位' ,type: "national", discipline: "boulder", detail: "0"},
+    { year: '2026', month:'2' , competition: 'ボルダージャパンカップ', description: '駒沢 7位' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2025', month:'6' , competition: 'ボルダーワールドカップ', description: 'インスブルック 10位' ,type: "international", discipline: "boulder", detail: "detail"},
+    { year: '2025', month:'2' , competition: 'ボルダージャパンカップ', description: '駒沢 4位' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2024', month:'8' , competition: '世界ユース選手権 リード', description: '貴陽 1位' ,type: "international", discipline: "lead", detail: "detail"},
+    { year: '2024', month:'5' , competition: '日本ユース選手権 リード', description: '盛岡 3位' ,type: "national", discipline: "lead", detail: "detail"},
+    { year: '2023', month:'12' , competition: 'アジアカップ ボルダー', description: 'リヤド 3位' ,type: "international", discipline: "boulder", detail: "detail"},
+    { year: '2023', month:'8' , competition: '世界ユース選手権 ボルダー', description: 'ソウル 4位' ,type: "international", discipline: "boulder", detail: "detail"},
+    { year: '2023', month:'6' , competition: '日本ユース選手権 ボルダー', description: '倉吉 2位' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2021', month:'12' , competition: '日本ユース選手権 ボルダー', description: '倉吉 1位' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2021', month:'8' , competition: '世界ユース選手権 スピード', description: 'ヴォロネジ 3位' ,type: "international", discipline: "speed", detail: "detail"},
+    { year: '2021', month:'3' , competition: '日本ユース選手権 スピード', description: '亀岡 1位' ,type: "national", discipline: "speed", detail: "detail"},
+    { year: '2020', month:'11' , competition: '日本ユース選手権 ボルダー', description: '葛飾 2位' ,type: "national", discipline: "boulder", detail: "detail"},
   ]
   const [hoveredIndex, setHoveredIndex] = useState<number|null>(null)
   const handleEventHover = (state: string, index: number) => {
@@ -266,35 +329,35 @@ export function PlayerTimeline() {
   }
 
   return (
-    <section id="player" className="my-prpl-bg section_detail">
-      <div className="container mx-auto px-4 max-w-4xl">
-        <h2>Main results</h2>
-        <div className="relative my-white-bg">
-          {/* 縦線 */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-0.5 bg-blue-500" />
-          {/* イベント */}
-          {events.map((event, index) => (
-            <div key={index}>
-              <div key={index} className="px-4 py-4 flex justify-between items-center items-stretch w-full">
-                {event.type === "national" ? (
-                  <>
-                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
-                    <PlayerTimeline_Dot/>
-                    <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
-                  </>
-                ) : (
-                  <>
-                    <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
-                    <PlayerTimeline_Dot/>
-                    <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
-                  </>
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
+    <div className="my-white-bg my-6">
+      <div className="py-9">
+        <h2>TimeLine</h2>
       </div>
-    </section>
+      <div className="relative">
+        {/* 縦線 */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-0.5 bg-blue-500" />
+        {/* イベント */}
+        {events.map((event, index) => (
+          <div key={index}>
+            <div key={index} className="px-4 py-4 flex justify-between items-center items-stretch w-full">
+              {event.type === "national" ? (
+                <>
+                  <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
+                  <PlayerTimeline_Dot/>
+                  <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
+                </>
+              ) : (
+                <>
+                  <PlayerTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
+                  <PlayerTimeline_Dot/>
+                  <PlayerTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
@@ -333,8 +396,8 @@ export function PlayerTimeline_Event({year, month, competition, description, dis
     onMouseLeave={() => onHoverChange("leave", index)}
     className={`w-11/24 ${left?"text-right pl-8":"text-left pr-8"}`}
     >
-      <h3 className={`text-2xl font-bold text-blue-600 ${discipline==='boulder'?"text-yellow-400":discipline==='lead'?"text-green-600":"text-red-400"}`}>{year + "." + month}</h3>
-      <h4 className={`text-lg font-semibold mt-2`}>{competition}</h4>
+      <h3 className={`text-3xl font-bold text-blue-600 ${discipline==='boulder'?"text-yellow-400":discipline==='lead'?"text-green-600":"text-red-400"}`}>{year + "." + month}</h3>
+      <h4 className={`text-xl font-semibold mt-2`}>{competition}</h4>
       <p className="text-gray-600 mt-1 text-xl">{description}</p>
     </div>
   )
@@ -342,6 +405,149 @@ export function PlayerTimeline_Event({year, month, competition, description, dis
 }
 
 interface PlayerTimeline_EventProps{
+  year: string
+  month: string
+  competition: string
+  description: string
+  discipline: string
+  left: boolean
+  onHoverChange: (state: string, index: number) => void
+  index: number
+}
+
+export function Setter(){
+  return(
+    <section id="setter" className="my-prpl-bg section_detail">
+      <div className="container mx-auto px-4 max-w-4xl">
+        <h1>Setter</h1>
+        <SetterStyle/>
+        <SetterTimeline/>
+      </div>
+    </section>
+  )
+}
+
+export function SetterStyle() {
+
+  return (
+    <div className="my-white-bg my-6">
+      <div className="py-9">
+        <h2>Style</h2>
+      </div>
+      <div className="flex justify-center items-center">
+          <div className="w-6/12 mx-4 my-4">
+            <p className="noto-serif">　2006年6月19日生まれ。2015年にクライミングを始め、翌年からクライミングスクールで本格的に練習を始める。<br/>　ユースC(現在でいうU14)からユース選手権に出場し、リードユース選手権は2度目、ボルダーユース選手権は3度目で決勝に進出する。</p>
+          </div>
+          <div className="w-6/12 mx-4 my-4">
+            <Image
+              src="/images/2023_B_BJC_S1.jpg"
+              alt="説明"
+              width={600}
+              height={400}
+              priority
+            />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export function SetterTimeline() {
+  const events = [
+    { year: '2025', month:'10' , competition: '高校生選手権 東海合同県予選会', description: 'チーフルートセッター' ,type: "national", discipline: "lead", detail: "detail"},
+    { year: '2025', month:'8' , competition: '全国中学生ボルダー競技大会', description: 'ルートセッター' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2024', month:'8' , competition: '全国中学生ボルダー競技大会', description: 'ルートセッター' ,type: "national", discipline: "boulder", detail: "detail"},
+    { year: '2024', month:'3' , competition: 'C級ルートセッター 取得', description: 'ライセンス' ,type: "national", discipline: "lead", detail: "detail"},
+  ]
+  const [hoveredIndex, setHoveredIndex] = useState<number|null>(null)
+  const handleEventHover = (state: string, index: number) => {
+    switch(state){
+      case "enter":
+        setHoveredIndex(index);
+        break;
+      case "leave":
+        setHoveredIndex(null);
+        break;
+    }
+  }
+
+  return (
+    <div className="my-white-bg my-6">
+      <div className="py-9">
+        <h2>TimeLine</h2>
+      </div>
+      <div className="relative">
+        {/* 縦線 */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-0.5 bg-blue-500" />
+        {/* イベント */}
+        {events.map((event, index) => (
+          <div key={index}>
+            <div key={index} className="px-4 py-4 flex justify-between items-center items-stretch w-full">
+              {event.type === "national" ? (
+                <>
+                  <SetterTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
+                  <SetterTimeline_Dot/>
+                  <SetterTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
+                </>
+              ) : (
+                <>
+                  <SetterTimeline_Detail hovered={hoveredIndex === index} detail={event.detail}/>
+                  <SetterTimeline_Dot/>
+                  <SetterTimeline_Event year={event.year} month={event.month} competition={event.competition} description={event.description} discipline={event.discipline} left={event.type === "national"} onHoverChange={handleEventHover} index={index}/>
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export function SetterTimeline_Detail({hovered, detail}: SetterTimeline_DetailProps){
+  return(
+    <div className="w-11/24 overflow-hidden rounded-lg">
+      {hovered ? (
+        <div className={"flex items-center justify-center relative h-full transition-all duration-300 cursor-pointer bg-white shadow-xl scale-105"}>
+          <p className={"mx-5 text-sm"}>{detail}</p>
+        </div>
+      ):(
+        <div/>
+      )}
+    </div>
+  )
+}
+
+interface SetterTimeline_DetailProps{
+  hovered: boolean
+  detail: string
+}
+
+export function SetterTimeline_Dot(){
+  return(
+    <div className="w-2/24 flex justify-center items-center">
+    <div className="w-4 h-4 bg-blue-500 rounded-full border-4 border-white shadow" />
+    </div>
+  )
+}
+
+
+export function SetterTimeline_Event({year, month, competition, description, discipline, left, onHoverChange, index}: SetterTimeline_EventProps){
+  return(
+    <div
+    onMouseEnter={() => onHoverChange("enter", index)}
+    onMouseLeave={() => onHoverChange("leave", index)}
+    className={`w-11/24 ${left?"text-right pl-8":"text-left pr-8"}`}
+    >
+      <h3 className={`text-3xl font-bold text-blue-600 ${discipline==='boulder'?"text-yellow-400":discipline==='lead'?"text-green-600":"text-red-400"}`}>{year + "." + month}</h3>
+      <h4 className={`text-xl font-semibold mt-2`}>{competition}</h4>
+      <p className="text-gray-600 mt-1 text-xl">{description}</p>
+    </div>
+  )
+
+}
+
+interface SetterTimeline_EventProps{
   year: string
   month: string
   competition: string
